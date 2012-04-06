d447f8e07f8d68fec49f4fe7cd7b11a05ce7d5ca
Oleksandr Vlasenko <ovlasenko@malkosua.com>
2012-04-06 15:47:22 +0300

    config recover

---
+++ config/autoload/global.config.php
@@ -0,0 +1,32 @@
+<?php
+
+  Global Configuration Override
+ 
+  You can use this file for overridding configuration values from modules, etc.  
+  You would place values in here that are agnostic to the environment and not 
+  sensitive to security. 
+ 
+  @NOTE In practice, this file will typically be INCLUDED in your source 
+  control, so do not include passwords or other sensitive information in this 
+  file.
+ 
+
+return array(
+    'di' = array(
+        'instance' = array(
+    'ZendDbAdapterAdapter' = array(
+                'parameters' = array(
+                    'driver' = array(
+                        'driver' = 'Pdo',
+                        'dsn' = 'mysqldbname=admin;hostname=localhost',
+                        'username' = 'root',
+                        'password' = '',
+                        'driver_options' = array(
+                            PDOMYSQL_ATTR_INIT_COMMAND = 'SET NAMES 'UTF8''
+                        ),
+                    ),
+                )
+            ),
+        )
+    )
+);
